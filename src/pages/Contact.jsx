ae433f998884293ce3321a85ee8ef956e1ca0962
asimboga <asimboga@gmail.com>
2023-03-10 20:56:12 +0300

    contact added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
-import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaYoutube, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const ContactForm = () => {
   const [formFields, setFormFields] = useState({
@@ -16,61 +16,64 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formFields); // Burada form alanlarını konsolda yazdırabilirsiniz
+    console.log(formFields); 
   };
 
   return (
-    <Container>
-      <Col md={6}>
-          <h2>Bize Ulaşın</h2>
-          <ul className="list-unstyled">
-            <li><FaPhone /> Telefon: +90 (212) 555 55 55</li>
-            <li><FaEnvelope /> E-posta: info@eticaretsitem.com</li>
-            <li><FaMapMarkerAlt /> Adres: İstanbul, Türkiye</li>
-          </ul>
-        </Col>
-      <Row className="justify-content-md-center">
-        <Col md={6}>
-          <h2 className="my-4 text-center">İletişim Formu</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formName">
-              <Form.Label>Adınız</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Adınız"
-                name="name"
-                value={formFields.name}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-              <Form.Label>E-Posta Adresiniz</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="E-Posta Adresiniz"
-                name="email"
-                value={formFields.email}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="formMessage">
-              <Form.Label>Mesajınız</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder="Mesajınız"
-                name="message"
-                value={formFields.message}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Gönder
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+<Container>
+  <Row className="">
+    <Col xs={12} md={6} className="p-5 text-center align-items-center justify-content-center">
+      <h2>Bize Ulaşın</h2>
+      <ul className="list-unstyled">
+        <li><FaYoutube /> Telefon: +90 (212) 555 55 55</li>
+        <li><FaTwitter /> Telefon: +90 (212) 555 55 55</li>
+        <li><FaInstagram /> Telefon: +90 (212) 555 55 55</li>
+        <li><FaPhone /> Telefon: +90 (212) 555 55 55</li>
+        <li><FaEnvelope /> E-posta: arskaciftlik@gmail.com</li>
+        <li><FaMapMarkerAlt /> Adres: Çanakkale, Türkiye</li>
+      </ul>
+    </Col>
+    <Col xs={12} md={6}>
+      <h2 className="my-4 text-center">İletişim Formu</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formName">
+          <Form.Label>Adınız</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Adınız"
+            name="name"
+            value={formFields.name}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>E-Posta Adresiniz</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="E-Posta Adresiniz"
+            name="email"
+            value={formFields.email}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formMessage">
+          <Form.Label>Mesajınız</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Mesajınız"
+            name="message"
+            value={formFields.message}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Gönder
+        </Button>
+      </Form>
+    </Col>
+  </Row>
+</Container>
   );
 };
 
